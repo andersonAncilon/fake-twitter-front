c96a21016e306ccddc960d8d844a60201b146a9d
disgraceTerror <dan100anderson@gmail.com>
2019-02-07 16:50:12 -0300

    All done

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,27 +1,53 @@
 import React, { Component } from "react";
 import "./Timeline.css";
 import twitterLogo from "../twitter.svg";
+import socket from "socket.io-client";
 
-import api from '../services/Api';
+import api from "../services/Api";
+import Tweet from "../components/Tweet";
 
 export default class Timeline extends Component {
   state = {
+    tweets: [],
     newTweet: ""
   };
 
-  handleNewTweet =  async (event) => {
-      if (event.keyCode != 13) return;
+  async componentDidMount() {
+    this.subscribeToEvents();
+    const response = await api.get("tweets");
 
-      const content = this.state.newTweet;
-      const author = window.localStorage.getItem("userName");
-
-      await api.post('tweets', {
-          author,
-          content
-      });
-
-      this.setState({ newTweet: "" })
+    this.setState({ tweets: response.data });
   }
+
+  subscribeToEvents = () => {
+    const io = socket("http://localhost:3000");
+
+    io.on("tweets", data => {
+      this.setState({ tweets: [data, ...this.state.tweets] });
+    });
+
+    io.on("likes", data => {
+      this.setState({
+        tweets: this.state.tweets.map(tweet =>
+          tweet._id === data._id ? data : tweet
+        )
+      });
+    });
+  };
+
+  handleNewTweet = async event => {
+    if (event.keyCode != 13) return;
+
+    const content = this.state.newTweet;
+    const author = window.localStorage.getItem("userName");
+
+    await api.post("tweets", {
+      author,
+      content
+    });
+
+    this.setState({ newTweet: "" });
+  };
 
   render() {
     return (
@@ -35,8 +61,11 @@ export default class Timeline extends Component {
             placeholder="What is hapenning?"
           />
         </form>
-
-
+        <ul className="tweet-list">
+          {this.state.tweets.map((tweet, key) => (
+            <Tweet key={tweet._id} tweet={tweet} />
+          ))}
+        </ul>
       </div>
     );
   }
--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseUrl: 'http://localhost:3001/'
+    baseURL: 'http://localhost:3000/'
 })
 
 export default api;
